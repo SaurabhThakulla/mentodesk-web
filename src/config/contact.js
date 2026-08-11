6ebbe1212a7c59324db7a9d754edaad8601b4af1
Saurabh <sauravthakulla683@gmail.com>
2026-08-11 23:17:26 +0530

    feat(config): add contact configuration

---
+++ src/config/contact.js
@@ -0,0 +1,9 @@
+export const CONTACT_INFO = {
+  address: "KR Puram, Bangalore 560036",
+  email: "contact@mentodesk.com",
+  phone: "+91 7639 143 881",
+  socials: {
+    instagram: "https://www.instagram.com/mentodesk?igsh=MTF2M2R0bDllcXJteg==",
+    linkedin: "https://www.linkedin.com/company/mentodesk"
+  }
+};

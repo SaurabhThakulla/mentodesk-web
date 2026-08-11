c0cb1a8c14a555931c84564df357cbc634991527
Saurabh <sauravthakulla683@gmail.com>
2026-08-11 23:17:27 +0530

    refactor(footer): import contact configuration

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logoImg from '../assets/logo.png';
 
+import { CONTACT_INFO } from '../config/contact';
+
 export default function Footer({ setCurrentView, onRequestDemo }) {
   const handleNav = (page) => {
     setCurrentView(page);
